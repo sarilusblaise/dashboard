6039000d350a98d322dd6bf6e61bc11eb5ec1629
Sarilus Blondy Wadley <sarilusblondywadley@gmail.com>
2024-02-09 21:29:01 -0500

    Streaming latestInvoices with react suspense.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+import { fetchCardData} from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton,LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
  
 export default async function Page() {
   //const revenue = await fetchRevenue();  fetchRevenue make to dashboard slow , we removed it from the dashboard and fetch it instead inside the RevenueChart and we used react suspense to streaming it.
-  const latestInvoices = await fetchLatestInvoices();
+  
   const {
       numberOfCustomers,
       numberOfInvoices,
@@ -35,7 +35,10 @@ export default async function Page() {
        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} /> 
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices /> 
+        </Suspense>
+        
       </div>
     </main>
   );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+     console.log('Fetching invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
