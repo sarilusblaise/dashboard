489999c4d84f4a09a69c00473d2eeab2eaa86fcb
Sarilus Blondy Wadley <sarilusblondywadley@gmail.com>
2024-02-14 22:20:35 -0500

    update the url with search parameter.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams,usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+ 
   function handleSearch(term:string){ 
-    console.log(term);
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
