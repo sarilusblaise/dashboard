4659b1f3e0bb037dea27ce90889ba9733a30d7bc
Sarilus Blondy Wadley <sarilusblondywadley@gmail.com>
2024-02-17 22:14:34 -0500

    debouncing test

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,6 +8,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
  
   function handleSearch(term:string){ 
+    console.log(`Searching... ${term}`);
+ 
     const params = new URLSearchParams(searchParams)
     if (term) {
       params.set('query', term);
