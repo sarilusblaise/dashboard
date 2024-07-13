d0bf2ecf0a4c6b96ef165de0cd5f29fa4f877e08
Sarilus Blondy Wadley <sarilusblondywadley@gmail.com>
2024-07-13 10:48:39 -0400

    inserted invoices into the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,25 @@
 'use server';
 
+import { z } from 'zod';
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
   // Test it out:
-  console.log(rawFormData);
+  //console.log(rawFormData);
 }
