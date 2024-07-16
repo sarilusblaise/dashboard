423597c98e3c20d141eff59f31a23682bb3f41aa
Sarilus Blondy Wadley <sarilusblondywadley@gmail.com>
2024-07-16 11:31:34 -0400

    updating and deleting invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,3 +32,34 @@ const { customerId, amount, status } = CreateInvoice.parse({
   // Test it out:
   //console.log(rawFormData);
 }
+
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
